5c8149cdfae21d4ccc31eb2f8009f57d3a2f61f1
Tommy Bennett <tommy_bennett@icloud.com>
2019-05-08 07:18:29 -0500

    The range did not include 100.

--- students/tommy_bennett/lesson2/fizz_buzz.py
+++ students/tommy_bennett/lesson2/fizz_buzz.py
@@ -1,11 +1,10 @@
-for i in range(100):
-    x = i + 1
+for i in range(1, 101):
     fizz_buzz = '' 
-    if x % 3 == 0:
+    if i % 3 == 0:
         fizz_buzz = "Fizz"
-    if x % 5 == 0:
+    if i % 5 == 0:
         fizz_buzz += "Buzz"
     if len(fizz_buzz):
         print(fizz_buzz)
     else:
-        print(x)
+        print(i)
